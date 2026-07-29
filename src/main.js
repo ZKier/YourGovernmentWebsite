9d0fa67ce0835e6b29bc4b8ac90cdae8f543008c
Zack <zackkier2006@gmail.com>
2026-07-29 19:09:20 -0400

    added geojson to requirements.txt and tried to improve the load time speed of the website by deleting .gpkg file

--- src/main.js
+++ src/main.js
@@ -25,8 +25,10 @@ const viewer = new Cesium.Viewer('map-container', {
 
 //viewer.resolutionScale = window.devicePixelRatio;
 //viewer.scene.fxaa = false;
+// Checking to see how long it takes for the total boundary to load
+console.time("total boundary load");
 
-
+console.time("fetch");
 const boundary = await Cesium.GeoJsonDataSource.load(
     '/data/us_nation.geojson',
     {
@@ -37,21 +39,33 @@ const boundary = await Cesium.GeoJsonDataSource.load(
     }
 );
 
+console.timeEnd("fetch");
+
 // Add the US GeoJSON boundary to the globe
 viewer.dataSources.add(boundary);
+console.timeEnd("total boundary load");
+
+// const entities = boundary.entities.values;
+
+// const state = await viewer.dataSourceDisplay.getBoundingSphere(
+//     entities[0],
+//     false
+// );
+
+await viewer.flyTo(boundary);
 
 // Have the camera fly to the US Center
-await viewer.camera.flyTo({
-  destination: usCenter,
+// await viewer.camera.flyTo({
+//   destination: usCenter,
 
-  orientation: {
-    heading: Cesium.Math.toRadians(0), // compass direction '0 = Facing North' every +90 is a clockwise turn so E S W
-    pitch: Cesium.Math.toRadians(-90), // Angle that you look at the planet, -90 is looking stright down
-    roll: Cesium.Math.toRadians(0), // Tilt, imagine tilting your head
-  },
+//   orientation: {
+//     heading: Cesium.Math.toRadians(0), // compass direction '0 = Facing North' every +90 is a clockwise turn so E S W
+//     pitch: Cesium.Math.toRadians(-90), // Angle that you look at the planet, -90 is looking stright down
+//     roll: Cesium.Math.toRadians(0), // Tilt, imagine tilting your head
+//   },
   
-  duration: 2.0,
-});
+//   duration: 2.0,
+// });
 
 // Add Cesium OSM Buildings, a global 3D buildings layer.
 //const buildingTileset = await createOsmBuildingsAsync();
--- vite.config.js
+++ vite.config.js
@@ -2,5 +2,12 @@ import { defineConfig } from 'vite';
 import cesium from 'vite-plugin-cesium';
 
 export default defineConfig({
-  plugins: [cesium()]
+  plugins: [cesium()],
+  server: {
+        watch: {
+            ignored: [
+                "**/*.gpkg",
+            ],
+        },
+    },
 });
