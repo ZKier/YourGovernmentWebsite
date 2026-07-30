866ffd8467d6a03be963f8cfefc3994734d0d495
Zack <zackkier2006@gmail.com>
2026-07-30 08:36:45 -0400

    updated the vite.config.js file to speed up website load time in docker.

--- vite.config.js
+++ vite.config.js
@@ -2,11 +2,22 @@ import { defineConfig } from 'vite';
 import cesium from 'vite-plugin-cesium';
 
 export default defineConfig({
-  plugins: [cesium()],
-  server: {
+    plugins: [cesium()],
+    server: {
         watch: {
+            usePolling: true,
+            interval: 500,
+
             ignored: [
+                "**/node_modules/**",
+                "**/.git/**",
+                "**/data/**",
+                "**/python/**",
+                "**/.venv/**",
                 "**/*.gpkg",
+                "**/*.geojson",
+                "**/*.sqlite",
+                "**/*.db",
             ],
         },
     },
