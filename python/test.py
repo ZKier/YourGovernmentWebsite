118d4edda148db4033461e8e6d52866c924880c8
Zack <zackkier2006@gmail.com>
2026-08-31 13:15:05 -0500

    Added state data geojson file, updated .gitignore to ignore .gpkg files, updated test.py to return state data at the time.

--- python/test.py
+++ python/test.py
@@ -8,20 +8,21 @@ print("Does this work here?")
 # project_root = ""
 
 # Windows
-gpkg_path = "public/data/cb_2025_us_all_5m/cb_2025_us_all_5m.gpkg"
+# gpkg_path = "public/data/cb_2025_us_all_5m/cb_2025_us_all_5m.gpkg"
+gpkg_path = "public/data/cb_2025_us_all_5m.gpkg"
 project_root = Path(__file__).resolve().parent.parent
 
 # Get the layers in the file
 layers = gpd.list_layers(gpkg_path)
 print(layers)
 
-gdf = gpd.read_file(gpkg_path, layer="cb_2025_us_nation_5m")
+gdf = gpd.read_file(gpkg_path, layer="cb_2025_us_state_5m")
 print(gdf.head())
 
 # Make sure it uses longitude and latitude
 gdf = gdf.to_crs("EPSG:4326")
 
-output_path = project_root / "data" / "us_nation.geojson"
+output_path = project_root / "data" / "us_state_data.geojson"
 
 gdf.to_file(output_path, driver="GeoJSON")
 
